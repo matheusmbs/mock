97f7a40e784aae1341a2df458e53287c227ed3bf
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2019-10-30 15:42:23 -0300

    arquivo midias

--- src/midias/midias.service.ts
+++ src/midias/midias.service.ts
@@ -11,7 +11,8 @@ export class MidiasService {
         listaMidias.push({
             nome: midia.originalname,
             id: listaMidias[listaMidias.length-1].id+1,
-            tempo: `0:${this.tempoAleatorio()}${this.tempoAleatorio()}`
+            tempo: `0:${this.tempoAleatorio()}${this.tempoAleatorio()}`,
+            file: "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3"
         })
     }
 
