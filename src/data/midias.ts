e9d2fffc8fa9dbadda972019d2a5c757b35a1d20
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2019-10-30 15:38:09 -0300

    midias

--- src/data/midias.ts
+++ src/data/midias.ts
@@ -2,32 +2,38 @@ export const midias = [
     {
         nome: "Som Back-end",
         tempo: "0:10",
-        id: 1
+        id: 1,
+        file: "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3"
     },
     {
         nome: "som",
         tempo: "0:15",
-        id: 2
+        id: 2,
+        file: "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3"
     },
     {
         nome: "audio-a",
         tempo: "0:14",
-        id: 3
+        id: 3,
+        file: "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3"
     },
     {
         nome: "audio-b",
         tempo: "0:14",
-        id: 4
+        id: 4,
+        file: "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3"
     },
     {
         nome: "audio-c",
         tempo: "0:14",
-        id: 5
+        id: 5,
+        file: "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3"
     },
     {
         nome: "audio-d",
         tempo: "0:25",
-        id: 6
+        id: 6,
+        file: "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3"
     }
 ];
 
